65254fb70a487fc40692e7a03a71bd5ff0a438f1
Aakash R <55597165+Aakash1616@users.noreply.github.com>
2022-02-02 17:37:55 +0530

    Time: 211 ms (25.52%), Space: 14.9 MB (85.09%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,10 +3,10 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         col = False
         for i in range(m):
-            if matrix[i][0] == 0:
+            if not matrix[i][0]:
                 col = True
             for j in range(1, n):
-                if matrix[i][j]==0:
+                if not matrix[i][j]:
                     matrix[0][j] = 0
                     matrix[i][0] = 0  
         for i in range(1, m):
