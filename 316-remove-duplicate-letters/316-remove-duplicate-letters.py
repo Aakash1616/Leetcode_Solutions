cb4449cdca86ceffcdbc9cc07b7062ce67bd424c
Aakash R <55597165+Aakash1616@users.noreply.github.com>
2022-03-18 18:26:33 +0530

    Time: 36 ms (93.32%), Space: 14 MB (54.66%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -2,14 +2,14 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         last_occ = {c : i for i, c in enumerate(s)}
         st = ["!"]
-        visited = [0]*26 
+        visited = set() 
         
         for i, ch in enumerate(s):
-            if visited[ord(ch)-97]: continue 
+            if ch in visited: continue 
             
             while(ch < st[-1] and last_occ[st[-1]]>i):
-                visited[ord(st.pop())-97] = 0 
+                visited.remove(st.pop()) 
                 
-            visited[ord(ch)-97] = 1 
+            visited.add(ch)  
             st.append(ch) 
         return "".join(st[1:])
