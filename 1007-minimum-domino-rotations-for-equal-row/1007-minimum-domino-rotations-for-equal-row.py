5de7f67099af76ec6c5ae3a3d84be1fc74abd4a0
Aakash R <55597165+Aakash1616@users.noreply.github.com>
2022-03-20 21:32:38 +0530

    Time: 1890 ms (19.44%), Space: 15.1 MB (44.98%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,20 +1,6 @@
 class Solution:
-    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        dic = defaultdict(lambda : 0)
-        val , mx = 0, 0 
-        top_, bottom_ = [0]*6, [0]*6
-        for i in range(len(tops)):
-            dic[tops[i]]+=1 
-            top_[tops[i]-1]+=1 
-            if bottoms[i]!=tops[i]:
-                dic[bottoms[i]]+=1
-            bottom_[bottoms[i]-1]+=1 
-            if dic[tops[i]]>mx:
-                mx = dic[tops[i]]
-                val = tops[i] 
-            if dic[bottoms[i]]>mx: 
-                mx = dic[bottoms[i]] 
-                val = bottoms[i]
-        if mx<len(tops):
-            return -1 
-        return len(tops)-max(top_[val-1], bottom_[val-1])
+    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
+        s = reduce(set.__and__, (set(d) for d in zip(A, B)))
+        if not s: return -1
+        x = s.pop()
+        return min(len(A) - A.count(x), len(B) - B.count(x))
