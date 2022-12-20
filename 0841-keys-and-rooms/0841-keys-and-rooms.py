4317f8c2c949404d8f2267d3ca697567932ab45a
Aakash R <55597165+Aakash1616@users.noreply.github.com>
2022-12-20 07:22:36 +0530

    Time: 129 ms (60.97%), Space: 14.3 MB (87.64%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        q = collections.deque(rooms[0]) 
+        vis = [0]*len(rooms) 
+        vis[0] = 1 
+        while(q):
+            ele = q.popleft() 
+            vis[ele] = 1 
+            for i in rooms[ele]:
+                if not vis[i]:
+                    q.append(i) 
+        if all(vis): return True 
+        return False 
